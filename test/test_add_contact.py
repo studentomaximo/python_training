cb40e6ea2225f239b82ecb731b476f7aab716bb3
maximuzzz <studentomaximo@gmail.com>
2020-01-19 14:50:53 -0800

    Created new file - session.py and improved code a little bit

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,17 +12,17 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(first_name="Maximum", middle_name="ab", last_name="Kane", nick="Strabo", title="SQA",
         company="Faceboof", address="234 Hacker Dr", home_phone="321", mobile_phone="312",
         work_phone="999", fax="006", email="e1@gmai.com", email2="e2@gmail.com", email3="e3@gmail.com",
         home_page="NA", address2="12345 Wed Dr", notes="NA"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(first_name="", middle_name="", last_name="", nick="", title="",
         company="", address="", home_phone="", mobile_phone="", work_phone="", fax="", email="", email2="", email3="",
         home_page="", address2="", notes=""))
-    app.logout()
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="Max", header="q123", footer="q123"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
