f3141beffe819d9e0942b884c11d1c2f7ff14639
maximuzzz <studentomaximo@gmail.com>
2020-01-20 17:22:13 -0800

    Created test_delete_first_contact (task #7 completed)

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture(scope = "session")
+@pytest.fixture
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
