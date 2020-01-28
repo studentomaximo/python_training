c283a65029d5c6a0e83584877a65934b5dfd618f
maximuzzz <studentomaximo@gmail.com>
2020-01-27 21:47:18 -0800

    Improved test_del_contact - added validation check

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,5 +1,7 @@
-
+from model.contact import Contact
 
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(last_name="Test"))
     app.contact.delete_first_contact()
