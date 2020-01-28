744f2ac24e7ba2a140f1812f6eb8b8c165b5fc36
maximuzzz <studentomaximo@gmail.com>
2020-01-27 21:54:11 -0800

    Improved test_delete_all_contacts - added validation check

--- test/test_delete_all_contacts.py
+++ test/test_delete_all_contacts.py
@@ -1,5 +1,7 @@
-
+from model.contact import Contact
 
 
 def test_delete_all_contacts(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(last_name="Test"))
     app.contact.delete_all_contacts()
