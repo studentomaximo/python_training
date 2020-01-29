038f3a3420e98db760fb171a9ed041c2c88f6eb1
maximuzzz <studentomaximo@gmail.com>
2020-01-28 19:54:38 -0800

    Improved test_modify for groups & contacts - added validation check

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,8 +2,12 @@ from model.contact import Contact
 
 
 def test_modify_contact_first_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(last_name="Test"))
     app.contact.modify_first_contact(Contact(first_name="New Firstname"))
 
 
 def test_modify_contact_last_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(last_name="Test"))
     app.contact.modify_first_contact(Contact(last_name="New Lastname"))
